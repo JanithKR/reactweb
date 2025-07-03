c9cedf22338f9bac755a60493e69be3b81d0c2d0
Jkr200210 <kavishanrandi@gmail.com>
2025-07-03 22:51:26 +0530

    some changes day 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,7 +49,7 @@ export default function Header() {
             rel="noopener noreferrer"
             className="text-sm font-semibold leading-6 text-foreground hover:text-primary transition-colors"
           >
-            Contract
+            Contract details
           </a>
         </div>
         <div className="flex flex-1 justify-end">
